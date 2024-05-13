deb61eea25daff506695a403ea92dcbddd1a5012
SNOW <visrut444@gmail.com>
2024-05-13 16:21:17 +0530

    encapsulation-solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+//        obj.name = "vishrut";//Error: name' has private access in 'com.driver.RWOnly'
+//        System.out.println(obj.name);//Error: name' has private access in 'com.driver.RWOnly'
+
+        obj.setname("vishrut");
+        System.out.println(obj.getname());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    public String getname(){
+        return name;
+    }
+    public void setname(String name){
+        this.name = name;
+    }
 }
